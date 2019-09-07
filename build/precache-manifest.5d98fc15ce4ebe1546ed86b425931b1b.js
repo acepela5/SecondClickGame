abce53c4e0b42feed428e6ab252b913d19493b62
unknown <galan1451@gmail.com>
2019-09-07 16:49:55 -0500

    added dot

---
+++ build/precache-manifest.5d98fc15ce4ebe1546ed86b425931b1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c81254a76bb66c2a79255307e94e2d",
+    "url": "/SecondClickGame/index.html"
+  },
+  {
+    "revision": "564822d377f76c5fa8f3",
+    "url": "/SecondClickGame/static/css/main.1521e376.chunk.css"
+  },
+  {
+    "revision": "5e83aeb0a3d3423cfe0e",
+    "url": "/SecondClickGame/static/js/2.80f08b19.chunk.js"
+  },
+  {
+    "revision": "564822d377f76c5fa8f3",
+    "url": "/SecondClickGame/static/js/main.29c49a5f.chunk.js"
+  },
+  {
+    "revision": "235d271ea242e9673277",
+    "url": "/SecondClickGame/static/js/runtime~main.df849c0d.js"
+  }
+]);
